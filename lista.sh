070bde3b282144c622f5490f11bd2eab1584e3bb
laajaxd <67248213+laajaxd@users.noreply.github.com>
2020-06-21 22:59:09 -0500

    Add files via upload

---
+++ lista.sh
@@ -0,0 +1,97 @@
+#! /bin/bash
+
+#limpia la pantalla
+clear
+
+#muestra un mensaje en la pantalla
+
+echo "hello edson"
+
+
+read -p "dele enter para ver al primer alumno"
+
+
+
+clear
+ 
+
+
+echo nombre="luis"
+echo edad="27"
+echo correo="luiscarlos@gmail.com"
+echo carrera="doctor"
+echo numero de celular="998986798"
+
+
+
+
+
+read -p "enter para ver al alumno numero 2 "
+
+
+clear
+ 
+
+
+echo nombre="raul"
+echo edad="19"
+echo correo="raulmijirro@gmail.com"
+echo carrera="estudiante de derecho"
+echo numero de celular="998345623"
+
+
+
+
+read -p "enter para ver al alumno numero 3 "
+
+
+clear
+ 
+
+
+echo nombre="manuel"
+echo edad="17"
+echo correo="manuelelaja@gmail.com"
+echo carrera="es un probe inutil xd"
+echo numero de celular="996456734"
+
+
+
+
+read -p "enter para ver al alumno numero 4 "
+
+
+clear
+ 
+
+
+echo nombre="pablo"
+echo edad="30"
+echo correo="pablodebackyordigan@gmail.com"
+echo carrera="actor"
+echo numero de celular="978054634"
+
+
+
+
+read -p "enter para ver al alumno numero 5"
+
+
+clear
+ 
+
+
+echo nombre="daniel"
+echo edad="22"
+echo correo="danidani@gmail.com"
+echo carrera="estudiante de ingienieria civil"
+echo numero de celular="987056436"
+
+
+
+
+
+
+
+
+
